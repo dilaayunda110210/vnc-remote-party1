077d9805f6e06b9f11effa3f4d4cf6a738b16a9b
dila ayunda <157418013+dilaayunda110210@users.noreply.github.com>
2024-07-05 18:49:21 +0000

    up

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,14 +1,27 @@
 export const vncList = [
-  { vncLink: "https://pvdwh8-3000.csb.app" },
-  { vncLink: "https://stx22c-3000.csb.app" },
-  { vncLink: "https://jnxv4q-3000.csb.app" },
-  { vncLink: "https://xtp738-3000.csb.app" },
-  { vncLink: "https://smnys4-3000.csb.app" },
-  { vncLink: "https://2zftys-3000.csb.app" },
-  { vncLink: "https://893zm8-3000.csb.app" },
-  { vncLink: "https://gyl3q2-3000.csb.app" },
-  { vncLink: "https://5pdqty-3000.csb.app" },
-  { vncLink: "https://6twtnn-3000.csb.app" },
-  { vncLink: "https://q6dl75-3000.csb.app" },
-  { vncLink: "https://jjhrgk-3000.csb.app" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
+  { vncLink: "" },
 ];
+
+// https://pvdwh8-3000.csb.app
+// https://stx22c-3000.csb.app
+// https://jnxv4q-3000.csb.app
+// https://xtp738-3000.csb.app
+// https://smnys4-3000.csb.app
+// https://2zftys-3000.csb.app
+// https://893zm8-3000.csb.app
+// https://gyl3q2-3000.csb.app
+// https://5pdqty-3000.csb.app
+// https://6twtnn-3000.csb.app
+// https://q6dl75-3000.csb.app
+// https://jjhrgk-3000.csb.app
